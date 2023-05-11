dd92845335a6b8dc0b718042a280efb46abb43c9
Gabbe2003 <gabriell.touma@gmail.com>
2023-05-11 22:26:36 +0200

    commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import PasswordGenerator from "./PassWord-generator.tsx";
+
+function App() {
+  return (
+    <>
+      <PasswordGenerator></PasswordGenerator>
+    </>
+  );
+}
+
+export default App;
